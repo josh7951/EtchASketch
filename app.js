eb0684dbcbe3b337effbefe61c16eb3f1b0bcddb
josh7951 <jmejia7951@gmail.com>
2019-11-15 21:54:37 -0800

    Full Version 1.0

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const container = document.querySelector("#container");
 makeGrid(16);
 makeColor('black');
 
+//Randomize Color Button
+const randomBtn = document.querySelector("#random-color");
+randomBtn.addEventListener("click", () => randomColor());
+//Choose Brush Color Button
+const chooseColorBtn = document.querySelector("#brush-color");
+chooseColorBtn.addEventListener("click", () =>{
+    let brushColor = prompt("Type your brush color (nothing too fancy, just generic colors) :)");
+    makeColor(brushColor);
+})
 // Clear grid button
 const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", () => clearGrid());
@@ -16,12 +25,24 @@ resizeBtn.addEventListener("click", () => {
         container.removeChild(container.firstChild);
     }
     do{
-        var newSize = parseInt(prompt("Enter a Grid Size: (Example: 16 --> creates 16x16 Grid)"));
+        var newSize = parseInt(prompt("Enter a Grid Size (1 - 150): \n(Example: 16 --> creates 16x16 Grid)"));
     }
-    while(isNaN(newSize));
-    makeGrid(newSize);
-    makeColor("black");
-
+    while(isNaN(newSize)){
+        if(newSize < 1){
+            alert("Grid Size CANNOT be smaller than 1!\nReverting back to default 16x16 size");
+            makeGrid(16);
+            makeColor('black');
+        }
+        else if(newSize > 150){
+            alert("Grid Size is too large!! Are you TRYING to freeze this webpage?!?\nReverting back to default 16x16 size");
+            makeGrid(16);
+            makeColor('black');
+        }
+        else{
+            makeGrid(newSize);
+            makeColor("black");
+        }
+    }
 });
 
 //Grid Function
@@ -55,4 +76,24 @@ function makeColor(color) {
             square.style.backgroundColor = color;
         });
     });
+}
+
+//color randomizer
+function randomColor() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = getRandomColor();
+        });
+    });
+}
+
+//random color function
+function getRandomColor() {
+    var value = '0123456789ABCDEF';
+    var hexCode = '#';
+    for ( var i = 0; i < 6; i++) {
+        hexCode += value[Math.floor(Math.random() * 16)];
+    }
+    return hexCode;
 }
